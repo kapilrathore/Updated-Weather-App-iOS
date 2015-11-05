60cb64bc33c546e09b477e5c2133076d201ca2ec
Kapil Rathore <krkapilrathore@gmail.com>
2015-11-06 02:05:43 +0530

    Create WeeklyTableViewController.swift

---
+++ Stormy/WeeklyTableViewController.swift
@@ -0,0 +1,158 @@
+//
+//  WeeklyTableViewController.swift
+//  Stormy
+//
+//  Created by Kapil Rathore on 05/11/15.
+//  Copyright © 2015 Kapil Rathore. All rights reserved.
+//
+
+import UIKit
+
+class WeeklyTableViewController: UITableViewController {
+
+    @IBOutlet weak var currentTemperatureLabel: UILabel?
+    @IBOutlet weak var currentWeatherIcon: UIImageView?
+    @IBOutlet weak var currentPrecipitationLabel: UILabel?
+    @IBOutlet weak var currentTemperatureRangeLabel: UILabel?
+    
+    // Location coordinates
+    let coordinate: (lat: Double, lon: Double) = (26.9000,75.8000)
+    // TODO: Enter your API key here
+    private let forecastAPIKey = "41db8bde58b6d9c75df0febc3fd218a0"
+    
+    var weeklyWeather: [DailyWeather] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        
+        retrieveWeatherForecast()
+    }
+    
+    func configureView() {
+        // Set table view's background view property
+        tableView.backgroundView = BackgroundView()
+        
+        // Set custom height for table view row
+        tableView.rowHeight = 64
+        
+        // Change the font and size of nav bar text
+        if let navBarFont = UIFont(name: "HelveticaNeue-Thin", size: 20.0) {
+            let navBarAttributesDictionary: [String: AnyObject]? = [
+                NSForegroundColorAttributeName: UIColor.whiteColor(),
+                NSFontAttributeName: navBarFont
+            ]
+            navigationController?.navigationBar.titleTextAttributes = navBarAttributesDictionary
+        }
+        
+        // Position refresh control above background view
+        refreshControl?.layer.zPosition = tableView.backgroundView!.layer.zPosition + 1
+        refreshControl?.tintColor = UIColor.whiteColor()
+    }
+    
+    @IBAction func refreshWeather() {
+        retrieveWeatherForecast()
+        refreshControl?.endRefreshing()
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDaily" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let dailyWeather = weeklyWeather[indexPath.row]
+                
+                (segue.destinationViewController as! ViewController).dailyWeather = dailyWeather
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Forecaste"
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return weeklyWeather.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("WeatherCell") as! DailyWeatherTableViewCell
+        
+        let dailyWeather = weeklyWeather[indexPath.row]
+        if let maxTemp = dailyWeather.maxTemperature {
+            cell.temperatureLabel.text = "\(maxTemp)º"
+        }
+        cell.weatherIcon.image = dailyWeather.icon
+        cell.dayLabel.text = dailyWeather.day
+        return cell
+    }
+    
+    // MARK: - Delegate Methods
+    
+    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tintColor = UIColor(red: 170/255.0, green: 131/255.0, blue: 224/255.0, alpha: 1.0)
+        
+        if let header = view as? UITableViewHeaderFooterView {
+            header.textLabel?.font = UIFont(name: "HelveticaNeue-Thin", size: 14.0)
+            header.textLabel?.textColor = UIColor.whiteColor()
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        cell?.contentView.backgroundColor = UIColor(red: 165/255.0, green: 142/255.0, blue: 203/255.0, alpha: 1.0)
+        let highlightView = UIView()
+        highlightView.backgroundColor = UIColor(red: 165/255.0, green: 142/255.0, blue: 203/255.0, alpha: 1.0)
+        cell?.selectedBackgroundView = highlightView
+    }
+    
+    // MARK: - Weather Fetching
+    
+    func retrieveWeatherForecast() {
+        let forecastService = ForecastService(APIKey: forecastAPIKey)
+        forecastService.getForecast(coordinate.lat, lon: coordinate.lon) {
+            (let forecaste) in
+            
+            if let weatherForecaste = forecaste,
+                let currentWeather = weatherForecaste.currentWeather {
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    if let temperature = currentWeather.temperature {
+                        self.currentTemperatureLabel?.text = "\(temperature)º"
+                    }
+                    
+                    if let precipitation = currentWeather.precipProbability {
+                        self.currentPrecipitationLabel?.text = "Rain: \(precipitation)%"
+                    }
+                    
+                    if let icon = currentWeather.icon {
+                        self.currentWeatherIcon?.image = icon
+                    }
+                    
+                    self.weeklyWeather = weatherForecaste.weekly
+                    
+                    if let hightTemp = self.weeklyWeather.first?.maxTemperature,
+                        let lowTemp = self.weeklyWeather.first?.minTemperature {
+                            self.currentTemperatureRangeLabel?.text = "↑\(hightTemp)º↓\(lowTemp)º"
+                    }
+                    
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+}
