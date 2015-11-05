51089bcf8e66cf44b922e7e3eff1ede377908d8b
Kapil Rathore <krkapilrathore@gmail.com>
2015-11-06 02:04:20 +0530

    Create Forecaste.swift

---
+++ Stormy/Forecaste.swift
@@ -0,0 +1,28 @@
+//
+//  Forecaste.swift
+//  Stormy
+//
+//  Created by Kapil Rathore on 05/11/15.
+//  Copyright © 2015 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct Forecaste {
+    
+    var currentWeather: CurrentWeather?
+    var weekly: [DailyWeather] = []
+    
+    init(weatherDictionary: [String: AnyObject]?) {
+        if let currentWeatherDictionary = weatherDictionary?["currently"] as? [String: AnyObject] {
+            currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
+        }
+        if let weeklyWeatherArray = weatherDictionary?["daily"]?["data"] as? [[String: AnyObject]] {
+            
+            for dailyWeather in weeklyWeatherArray {
+                let daily = DailyWeather(dailyWeatherDict: dailyWeather)
+                weekly.append(daily)
+            }
+        }
+    }
+}
